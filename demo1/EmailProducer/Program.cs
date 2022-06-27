1028598f6a14f3570560e7738a243561ad6e3d42
Jeffry Gonzalez <jeff@hypertheory.com>
2022-06-27 15:46:30 -0400

    demo of basic tye and dapr thing done

--- demo1/EmailProducer/Program.cs
+++ demo1/EmailProducer/Program.cs
@@ -1,35 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Dapr.Client;
+
 Console.WriteLine("Send a Message!");
 
 
 //
-var client = new HttpClient();
-client.BaseAddress = new Uri("http://localhost:1337");
+//var client = new HttpClient();
+//client.BaseAddress = new Uri("http://localhost:1337");
+using var client = new DaprClientBuilder().Build();
 while(true)
 {
-    Console.Write("What is the email address?: ");
-    var email = Console.ReadLine();
+   
 
-    Console.Write("What is the message?: ");
-    var message = Console.ReadLine();
+    var messageToSend = new EmailMessage("jeff@aol.com", "It is now " + DateTime.Now.ToLongTimeString());
 
-    var messageToSend = new EmailMessage(email, message);
-    var httpMessageBody = new HttpRequestMessage();
-    var json = System.Text.Json.JsonSerializer.Serialize(messageToSend);
-    httpMessageBody.Content = new StringContent(json);
-    httpMessageBody.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
-    // call the service.
 
-    var response = client.PostAsync("/messages", httpMessageBody.Content).Result;
-    // inspect the response... 
-    if(response.IsSuccessStatusCode)
-    {
-        Console.WriteLine("Your Message Was Sent");
+    await client.PublishEventAsync("emails", "email-requested", messageToSend);
 
-    } else
-    {
-        Console.WriteLine($"Got a {response.StatusCode}. Sorry");
-    }
+    await Task.Delay(3000);
+   
 }
 
 public record EmailMessage(string emailAddress, string message);
