34343ec4888f077718a25fa9167dc86b4a04c866
Jeffry Gonzalez <jeff@hypertheory.com>
2022-06-29 16:42:23 -0400

    done with the grpc demo

---
+++ services/GrpcDemoStuff/GrpcClientApp/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Core;
+using Grpc.Net.Client;
+
+Console.WriteLine("Hello, World!");
+
+
+
+// one channel per server, you can use the same channel for all services on that server
+var channel = GrpcChannel.ForAddress("https://localhost:4443");
+
+var client = new GrpcServer.Greeter.GreeterClient(channel);
+var topicClient = new GrpcServer.NotificationsService.NotificationsServiceClient(channel);
+
+Console.Write("What is your name? :");
+var name = Console.ReadLine();
+
+var reply = await client.SayHelloAsync(new GrpcServer.HelloRequest { Name = name });
+
+Console.WriteLine($"The Server said {reply.Message}");
+
+Console.WriteLine("What topic do you want to 'subscribe to'? ");
+var topic = Console.ReadLine();
+
+using var streamingCall = topicClient.GetNotificationsForTopic(
+    new GrpcServer.TopicRequest { TopicName = topic });
+
+
+await foreach( var response in streamingCall.ResponseStream.ReadAllAsync() )
+{
+    Console.WriteLine(response.Message);
+}
---
+++ services/GrpcDemoStuff/GrpcServer/Services/NotificationService.cs
@@ -0,0 +1,17 @@
+﻿using Grpc.Core;
+
+namespace GrpcServer.Services;
+
+public class NotificationService : NotificationsService.NotificationsServiceBase
+{
+    public override async Task GetNotificationsForTopic(TopicRequest request, IServerStreamWriter<TopicResponse> responseStream, ServerCallContext context)
+    {
+        var message = request.TopicName;
+
+        while(!context.CancellationToken.IsCancellationRequested)
+        {
+           // await Task.Delay(new Random().Next(1000, 5000));
+            await responseStream.WriteAsync(new TopicResponse { Message = $"Your Topic Said ${DateTime.Now}" });
+        }
+    }
+}
